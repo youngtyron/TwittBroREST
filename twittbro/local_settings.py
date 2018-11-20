79c6f44f8ee47458dacaba2c79224d3103cf7bd9
youngtyron <gro-jorge@mail.ru>
2018-11-20 20:31:53 +0500

    Start project with django, django-rest-framework and vue.js

---
+++ twittbro/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twittbro_base',
+        'USER': 'twittbro_user',
+        'PASSWORD': '1234',
+        'HOST':'127.0.0.1',
+        'PORT': '',
+    }
+}
